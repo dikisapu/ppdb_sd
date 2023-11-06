0ab1343553fce7edb362cb2e50618454c161ef94
dikisapu <dikis7256@gmail.com>
2023-11-06 11:23:00 +0700

    firts commit ppdb dini

---
+++ view/template/footer.php
@@ -0,0 +1,17 @@
+          
+      </div>
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; 2023 <div class="bullet"></div> Design By <a href="#">Dini Pratiwi</a> | Repost by <a href='#' title='author' target='_blank'>Authorsan</a>
+          
+        </div>
+        <div class="footer-right">
+          Developer : <a href="//linkedin.com/in/bejosuseno" target="blank">Codesan</a> 
+          <!-- Create By <a href="#">Code Peka</a> -->
+        </div>
+      </footer>
+    </div>
+  </div>
+
+</body>
+</html>
